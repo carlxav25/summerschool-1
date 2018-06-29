d42f7cc70032e2bf145343ac879e9db75d4c4150
training069 Student <training069@sisu-login3.csc.fi>
2018-06-29 10:09:01 +0300

    OpenMP hello world update 29/6/18

--- openmp/hello-world/hello.F90
+++ openmp/hello-world/hello.F90
@@ -1,9 +1,11 @@
 program hello
+ use omp_lib
   implicit none
-
-  print *, 'Hello world!'
-  !$omp parallel
-  print *, 'X'
+  integer :: omp_rank
+  print *, 'Hello world before thread!'
+  !$omp parallel private(omp_rank)
+   omp_rank = omp_get_thread_num()
+  print *, 'X','', omp_rank
   !$omp end parallel
 
 end program hello
